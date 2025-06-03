ab7828cdf17bdc092cf94aeea1fb8a8b8d249b55
FranParedesNavarrete <fparedesnav@gmail.com>
2025-06-03 18:51:51 +0200

    Modal para editar producto añadido

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -102,99 +102,99 @@
 
     <!-- Modal para editar producto -->
     <div class="modal fade" id="editProductModal" tabindex="-1" aria-labelledby="editProductModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="editProductModalLabel">Editar Producto</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form action="{{ route('products.update', $product->slug) }}" method="POST" enctype="multipart/form-data">
-                            @csrf
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editProductModalLabel">Editar Producto</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('products.update', $product->slug) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
 
-                            <div class="form-group pb-2">
-                                <label for="name">Nombre del producto <span class="text-danger">*<span></label>
-                                <input type="text" name="name" id="name" value="{{ $product->name }}" class="form-control" required>
-                                @error('name')
+                        <div class="form-group pb-2">
+                            <label for="name">Nombre del producto <span class="text-danger">*<span></label>
+                            <input type="text" name="name" id="name" value="{{ $product->name }}" class="form-control" required>
+                            @error('name')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-end pb-2">
+                            <div class="form-group w-50 pb-2">
+                                <label for="category">Categoría <span class="text-danger">*<span></label>
+                                <select name="category" id="category" class="form-control" required>
+                                    <option value="{{ $product->category }}" {{ old('category') ? '' : 'selected' }}>{{ ucfirst($product->category) }}</option>
+                                    @foreach($categories as $category)
+                                        @if ($category != $product->category)
+                                            <option value="{{ $category }}" {{ old('category') == $category ? 'selected' : '' }}>{{ ucfirst($category) }}</option>
+                                        @endif
+                                    @endforeach
+                                </select>
+                                @error('category')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
 
-                            <div class="d-flex justify-content-between align-items-end pb-2">
-                                <div class="form-group w-50 pb-2">
-                                    <label for="category">Categoría <span class="text-danger">*<span></label>
-                                    <select name="category" id="category" class="form-control" required>
-                                        <option value="{{ $product->category }}" {{ old('category') ? '' : 'selected' }}>{{ ucfirst($product->category) }}</option>
-                                        @foreach($categories as $category)
-                                            @if ($category != $product->category)
-                                                <option value="{{ $category }}" {{ old('category') == $category ? 'selected' : '' }}>{{ ucfirst($category) }}</option>
-                                            @endif
-                                        @endforeach
-                                    </select>
-                                    @error('category')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="form-group w-5 ps-2 pb-2">
-                                    <label for="price">Precio <span class="text-danger">*<span></label>
-                                    <input type="number" step="0.01" min="0" name="price" id="price" value="{{ $product->price }}" class="form-control" required>
-                                    @error('price')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
-                                </div>
+                            <div class="form-group w-5 ps-2 pb-2">
+                                <label for="price">Precio <span class="text-danger">*<span></label>
+                                <input type="number" step="0.01" min="0" name="price" id="price" value="{{ $product->price }}" class="form-control" required>
+                                @error('price')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
+                        </div>
 
-                            <div class="d-flex justify-content-between align-items-end pb-2">
-                                <div class="form-group w-50 pb-2">
-                                    <label for="state">Estado <span class="text-danger">*<span></label>
-                                    <select name="state" id="state" class="form-control" required>
-                                        <option value="{{ $product->state }}" {{ old('state') ? '' : 'selected' }}>{{ ucfirst($product->state) }}</option>
-                                        @foreach($states as $state)
-                                            <option value="{{ $state }}" {{ old('state') == $state ? 'selected' : '' }}>{{ ucfirst($state) }}</option>
-                                        @endforeach
-                                    </select>                     
-                                    @error('state')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
-                                </div>
-
-                                <div class="form-group w-5 ps-2 pb-2">
-                                    <label for="stock">Stock <span class="text-danger">*<span></label>
-                                    <input type="integer" min="0" name="stock" id="stock" value="{{ $product->stock }}" class="form-control" required>
-                                    @error('stock')
-                                        <div class="text-danger">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group pb-2">
-                                <label for="description">Descripción <span class="text-danger">*<span></label>
-                                <textarea maxlength="255" rows="3" name="description" id="description" class="form-control" required>{{ $product->description }}</textarea>
-                                @error('description')
+                        <div class="d-flex justify-content-between align-items-end pb-2">
+                            <div class="form-group w-50 pb-2">
+                                <label for="state">Estado <span class="text-danger">*<span></label>
+                                <select name="state" id="state" class="form-control" required>
+                                    <option value="{{ $product->state }}" {{ old('state') ? '' : 'selected' }}>{{ ucfirst($product->state) }}</option>
+                                    @foreach($states as $state)
+                                        <option value="{{ $state }}" {{ old('state') == $state ? 'selected' : '' }}>{{ ucfirst($state) }}</option>
+                                    @endforeach
+                                </select>                     
+                                @error('state')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
 
-                            <div class="form-group">
-                                <label class="d-flex justify-content-between" for="images">Imagenes <i class="bi bi-info-circle-fill text-info" data-bs-toggle="tooltip" data-be-placement="top" title="Subir nuevas imagenes reemplará las anteriores."></i></label>
-                                <input type="file" name="images[]" id="images" class="form-control" multiple>
-                                @error('images')
-                                    <div class="text-danger">{{ $message }}</div>
-                                @enderror
-                                @error('images.*')
+                            <div class="form-group w-5 ps-2 pb-2">
+                                <label for="stock">Stock <span class="text-danger">*<span></label>
+                                <input type="integer" min="0" name="stock" id="stock" value="{{ $product->stock }}" class="form-control" required>
+                                @error('stock')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
+                        </div>
 
-                            <br>
-                            <input type="submit" class="btn btn-primary" value="Guardar">
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                    </div>
+                        <div class="form-group pb-2">
+                            <label for="description">Descripción <span class="text-danger">*<span></label>
+                            <textarea maxlength="255" rows="3" name="description" id="description" class="form-control" required>{{ $product->description }}</textarea>
+                            @error('description')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label class="d-flex justify-content-between" for="images">Imagenes <i class="bi bi-info-circle-fill text-info" data-bs-toggle="tooltip" data-be-placement="top" title="Subir nuevas imagenes reemplará las anteriores."></i></label>
+                            <input type="file" name="images[]" id="images" class="form-control" multiple>
+                            @error('images')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                            @error('images.*')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <br>
+                        <input type="submit" class="btn btn-primary" value="Guardar">
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                 </div>
             </div>
         </div>
+    </div>
 @endsection
